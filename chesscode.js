6a04cc47bcef9c846b7591c2e6140e9d50389e93
spoorthi222 <spoorthi.n222@gmail.com>
2019-09-06 20:06:57 +0530

    chess

---
+++ chesscode.js
@@ -0,0 +1,29 @@
+
+    var black = "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>";
+    var white = "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>\n" +
+        "<div class='box color-white'></div>\n" +
+        "<div class='box color-black'></div>";
+
+    document.getElementById("a").innerHTML = black;
+    document.getElementById("b").innerHTML = white;
+    document.getElementById("c").innerHTML = black;
+    document.getElementById("d").innerHTML = white;
+    document.getElementById("e").innerHTML = black;
+    document.getElementById("f").innerHTML = white;
+    document.getElementById("g").innerHTML = black;
+    document.getElementById("h").innerHTML = white;
+
+
+
